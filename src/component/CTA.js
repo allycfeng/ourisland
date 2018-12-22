77679b3e952f6761b1faf508d9e15fbac9abe583
allycfeng <76328ching@gmail.com>
2018-12-23 06:42:25 +0800

    test

--- src/component/CTA.js
+++ src/component/CTA.js
@@ -10,8 +10,8 @@ class CTA extends Component {
       <section id="cta" className="bg-near-white pv6-l pv4">
         <div className="mw8 center ph3">
           <div className="cf ph2-ns tc hide">
-            <h1 className="ph2 fw7 tracked">這是標題</h1>
-            <h3 className="ph2 mb5-l mb4 fw4">這是副標</h3>
+            <h1 className="ph2 fw7 tracked">這是標題GAAAA</h1>
+            <h3 className="ph2 mb5-l mb4 fw4">這是副標GAAAA</h3>
             <div className="fl w-third-l w-100 pa2 cp">
               <div className="pv3 pa4 tc ctaBox bg-white">
                 <figure className="w5 h5 center mv0 flex aic jcc">
